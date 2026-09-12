49ebb36962b7f47361eebdbcac213ab482696750
harboot <harboot@gmail.com>
2026-09-12 14:57:49 +0700

    Replace matcher canvas with interactive chart

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Increment this value whenever any cached application file changes.
-const CACHE_VERSION = 'dlp-analyzer-v15';
+const CACHE_VERSION = 'dlp-analyzer-v16';
 const APPLICATION_FILES = [
   './AlertAnalyzer.html',
   './CardManager.html',
