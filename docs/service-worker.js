cbf2dc8fb6decbb03c1fac3cc9e747a6851bdce9
harboot <harboot@gmail.com>
2026-09-13 07:05:52 +0700

    Show risk settings in modal box

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Increment this value whenever any cached application file changes.
-const CACHE_VERSION = 'dlp-analyzer-v24';
+const CACHE_VERSION = 'dlp-analyzer-v25';
 const APPLICATION_FILES = [
   './AlertAnalyzer.html',
   './base.css',
