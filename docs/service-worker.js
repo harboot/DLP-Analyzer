8472ed891adaad019df3c27db5a869ab6fe69217
harboot <harboot@gmail.com>
2026-09-19 10:49:45 +0700

    Refine Policy Viewer resource summaries (#86)

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Increment this value whenever any cached application file changes.
-const CACHE_VERSION = 'dlp-analyzer-v67';
+const CACHE_VERSION = 'dlp-analyzer-v68';
 const CACHE_CONCURRENCY = 6;
 const APPLICATION_FILES = [
   './AlertAnalyzer.html',
