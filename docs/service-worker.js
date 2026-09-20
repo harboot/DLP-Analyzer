2a65b30807f961d106137a90c37692b1190a6ce3
harboot <harboot@gmail.com>
2026-09-20 13:12:31 +0700

    Align load logs and alert card grid (#97)

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -3,7 +3,7 @@
 importScripts('./js/detectors/index.js');
 
 // Increment this value whenever any cached application file changes.
-const CACHE_VERSION = 'dlp-analyzer-v81';
+const CACHE_VERSION = 'dlp-analyzer-v82';
 const CACHE_CONCURRENCY = 6;
 const APPLICATION_FILES = [
   './AlertAnalyzer.html',
